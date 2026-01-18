6b7e3f70012e51c559ebb6169a484ac8c0ca4016
Bruno <soubruno@protonmail.com>
2026-01-18 20:57:22 -0300

    feat: solve recursion bug in list procedure

--- src/client/bencode_parser.py
+++ src/client/bencode_parser.py
@@ -1,13 +1,12 @@
 from copy import deepcopy
-from stack import Stack
+from typing import Any
 
 class BencodeParser:
-    stack = Stack()
-    main_dictionary = {}
-    template_list = []
-    is_key = True # checks if entry is key or value in dictionary
-    key = ""
-    MAX_PRINTABLE = 127  # maximum possible decimal value for a printable char in the ASCII table
+    main_dictionary: dict = {}
+    is_key: bool = True # checks if entry is key or value in dictionary
+    key: Any = None
+    MAX_PRINTABLE: int = 127  # maximum possible decimal value for a printable char in the ASCII table
+    LAST_ADDED_KEY: Any = None
 
     @staticmethod
     def byte_string(file_content : str, index : int) -> list:
@@ -22,6 +21,8 @@ class BencodeParser:
         for i in range(length):
             string += file_content[index]
             index += 1
+
+        # this method points correctly to the next character
         return [string, index]
 
     @staticmethod
@@ -38,46 +39,38 @@ class BencodeParser:
     def decider(file_content : str, index : int):
         if file_content[index] == 'd':
             index += 1
-
             key = BencodeParser.decider(file_content, index)
-            index = key[1]
-
-            value = BencodeParser.decider(file_content, index)
+            value = BencodeParser.decider(file_content, key[1])
             index = value[1]
-
             return_value = {
                 key[0] : value[0]
             }
-
             return [return_value, index]
 
         elif file_content[index] == 'l':
             index += 1
+            local_list = []
 
             while file_content[index] != 'e':
-                return_value = BencodeParser.decider(file_content, index)
-                index = return_value[1]
-                BencodeParser.template_list.append(return_value[0])
+                returned_value = BencodeParser.decider(file_content, index)
+                local_list.append(returned_value[0])
+                index = returned_value[1]
+
             index += 1
-
-            aux = deepcopy(BencodeParser.template_list)
-            BencodeParser.template_list.clear()
-
-            return [aux, index] # -> [[], i]
+            print(local_list)
+            return [local_list, index]
 
         elif file_content[index] == 'i':
             returned_value = BencodeParser.integer(file_content, index) # -> [integer, i]
-            index = returned_value[1]
             return returned_value
 
-        elif file_content[index].isnumeric():
+        else:
             returned_value = BencodeParser.byte_string(file_content, index) # -> ["bencode", i]
-            index = returned_value[1]
             return returned_value
 
     @staticmethod
-    def decode(filename : str) -> dict:
-        with open(filename, "rb") as file: # test_file.torrent
+    def decode(filename : str):
+        with (open(filename, "rb") as file): # test_file.torrent
             file_content = ""
 
             for line in file.read(): # reads characters of torrent until it finds a non-printable character
@@ -95,12 +88,12 @@ class BencodeParser:
                     BencodeParser.key = returned_value[0]
                     BencodeParser.is_key = False
                 else:
-                    BencodeParser.is_key = True
                     BencodeParser.main_dictionary[BencodeParser.key] = returned_value[0]
+                    BencodeParser.is_key = True
 
                 index = int(returned_value[1])
 
         return BencodeParser.main_dictionary
 
 BencodeParser.decode(input())
-print(BencodeParser.main_dictionary)
+# print(BencodeParser.main_dictionary)
