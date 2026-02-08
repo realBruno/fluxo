99ae12d07ae04f36017e7b1577e9088588192d7e
Bruno <soubruno@protonmail.com>
2026-02-08 19:51:23 -0300

    refactor: integrate interface into main

--- main.py
+++ main.py
@@ -3,7 +3,33 @@
     Author: Bruno Fernandes (github.com/realBruno)
     Date: 24/jan/2026
 """
+import sys
 
-from client import interface
+from client import bencode, modes, peer, support
 
-interface.get_path()
+def make_request(path: str):
+    print("Parsing metadata...", end = '')
+    decoded, info_hash, is_single = support.get_file_info(path)
+    announce = decoded[b"announce"]
+    if is_single:
+        print("\rBuilding payload...", end = '')
+        payload = modes.single_torrent(decoded, info_hash)
+        print("\rSubmitting request...", end = '')
+        response = modes.submit(announce, payload)
+        print("\rRequest successful!", end = '')
+        response = bencode.parse(response, 0)[0]
+        print("\rStarting handshake with peers...", end = '')
+        peer.handshake(response)
+    else:
+        raise TypeError("Multi-file mode is under development")
+
+def get_path():
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+    elif len(sys.argv) == 1:
+        path = input("Path to file: ")
+    else:
+        raise TypeError(f"Takes exactly 1 argument. {len(sys.argv) - 1} given.")
+    make_request(path)
+
+get_path()
